04402635aa6e14257490729b08a3f5a8e2862ad6
Hari Vardhan Reddy Peddireddy <Harivardhanreddy.Peddireddy@blueyonder.com>
2025-03-04 19:22:07 +0530

    Added Bubble Sort and BinarySearch Java

--- DSA/src/linearSearch.java
+++ DSA/src/linearSearch.java
@@ -1,16 +1,17 @@
 public class linearSearch {
     public static void main(String[] args) {
-        int nums[] = {1,2,3,4,5,6,7,8,9};
+//        int nums[] = {1,2,3,4,5,6,7,8,9};
+        int nums[] = new int[10000];
         int target = 3;
         int root = linearsearch(nums,target);
 
         if(root != -1)
-            System.out.println(root);
+            System.out.println("Root value :"+ root);
         else
             System.out.println("Element Not Found  .. ");
 
-        int root1 = binarySearch(nums,target,0,0);
-        System.out.println(root1);
+//        int root1 = binarySearch(nums,target,0,0);
+//        System.out.println(root1);
 
     }
     public static int linearsearch(int []nums,int target){
@@ -23,27 +24,29 @@ public class linearSearch {
                 return target = i;
             }
         }
+        System.out.println("Steps Taken Linear Search : "+ steps);
         return -1;
     }
-    public static int binarySearch(int []nums,int target,int left,int right){
-        // 3,4,1,7,8,9,4
-        left = 0;
-        right = nums.length -1 ;
-        int steps = 0;
-
-        while(left <= right) {
-            int mid = (right+left)/2;
-
-            steps++;
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] < target) {
-                return binarySearch(nums,mid,mid+1,right);
-            } else {
-                return binarySearch(nums,mid,left,mid-1);
-            }
-        }
-        System.out.println("Steps Taken : "+ steps);
-        return -1;
-    }
+//    public static int binarySearch(int []nums,int target,int left,int right){
+//        // 3,4,1,7,8,9,4
+//        left = 0;
+//        right = nums.length -1 ;
+//        int steps = 0;
+//
+//        while(left <= right) {
+//            steps++;
+//            int mid = (right+left)/2;
+//
+//            if (nums[mid] == target) {
+//                System.out.println("Steps Taken : "+ steps);
+//                return mid;
+//            } else if (nums[mid] < target) {
+//                return binarySearch(nums,mid,mid+1,right);
+//            } else {
+//                return binarySearch(nums,mid,left,mid-1);
+//            }
+//        }
+//        System.out.println("Steps Taken : "+ steps);
+//        return -1;
+//    }
 }
